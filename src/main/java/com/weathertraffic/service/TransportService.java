49a371facb3b38d1be7db34fc4edc4626d436133
dvrisa <38985578+Riku-S@users.noreply.github.com>
2024-09-27 21:00:53 +0300

    Request data from digitraffic for transport data. Just print the json to console for now

--- src/main/java/com/weathertraffic/service/TransportService.java
+++ src/main/java/com/weathertraffic/service/TransportService.java
@@ -1,13 +1,26 @@
 package com.weathertraffic.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.weathertraffic.model.TransportStatus;
 import org.springframework.stereotype.Service;
+
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class TransportService {
     // API key, urls, etc...
-    
+    private static final String DIGITRAFFIC_API_URL = "https://tie.digitraffic.fi/api/tms/v1/sensors";
+
     public TransportStatus getTransport(String city) {
+        final WebClient client = WebClient.builder()
+                .defaultHeader("Digitraffic-User", "DT/Tester")
+                .build();
+
+        final JsonNode response = client.get().uri("https://tie.digitraffic.fi/api/tms/v1/sensors")
+                .retrieve().bodyToMono(JsonNode.class).block();
+
+        System.out.println(response);
+
         // Call the api and return the response body
         TransportStatus exampleStatus = new TransportStatus();
         exampleStatus.setDescription(String.format("Example transport status for %s", city));
