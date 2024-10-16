ee45bf7a74462d315ec04416f7a7f08eb4396526
dvrisa <38985578+Riku-S@users.noreply.github.com>
2024-10-16 14:01:56 +0300

    Refactor repeated code

--- src/main/java/com/weathertraffic/service/WeatherService.java
+++ src/main/java/com/weathertraffic/service/WeatherService.java
@@ -24,14 +24,15 @@ public class WeatherService {
                     "&storedquery_id=fmi::observations::weather::timevaluepair&place={city}" +
                     "&parameters=temperature,windspeedms&starttime={starttime}&endtime={endtime}";
 
-    private Measurement<Float> parseTemperature(Document document) {
+    private Measurement<Float> parseMeasurement(Document document, String parameter)
+    {
         // Parse temperature values
-        float temperature = 0f;
-        String temperatureTime = "Unknown";
+        float measuredValue = 0f;
+        String measurementTime = "Unknown";
         NodeList measurementNodes = document.getElementsByTagName("wml2:MeasurementTimeseries");
         for (int i = 0; i < measurementNodes.getLength(); i++) {
             String description = measurementNodes.item(i).getAttributes().getNamedItem("gml:id").getTextContent();
-            if (description.contains("temperature")) {
+            if (description.contains(parameter)) {
                 NodeList temperatureValues = measurementNodes.item(i).getChildNodes();
                 for (int j = 0; j < temperatureValues.getLength(); j++) {
                     if (temperatureValues.item(j).getNodeName().equals("wml2:point")) {
@@ -44,45 +45,15 @@ public class WeatherService {
                                 String value = pointNodes.item(k).getChildNodes().item(3).getTextContent();
                                 System.out.println("Temperature at " + time + ": " + value + " °C");
 
-                                temperatureTime = time;
-                                temperature = Float.parseFloat(value);
+                                measurementTime = time;
+                                measuredValue = Float.parseFloat(value);
                             }
                         }
                     }
                 }
             }
         }
-        return new Measurement<Float>(temperature, temperatureTime);
-    }
-
-    private Measurement<Float> parseWindSpeed(Document document) {
-        float windSpeed = 0f;
-        String windTime = "Unknown";
-        NodeList measurementNodes = document.getElementsByTagName("wml2:MeasurementTimeseries");
-        for (int i = 0; i < measurementNodes.getLength(); i++) {
-            String description = measurementNodes.item(i).getAttributes().getNamedItem("gml:id").getTextContent();
-            if (description.contains("windspeedms")) {
-                NodeList windSpeedValues = measurementNodes.item(i).getChildNodes();
-                for (int j = 0; j < windSpeedValues.getLength(); j++) {
-                    if (windSpeedValues.item(j).getNodeName().equals("wml2:point")) {
-                        NodeList pointNodes = windSpeedValues.item(j).getChildNodes();
-                        System.out.println("wml2:point found");
-                        for (int k = 0; k < pointNodes.getLength(); k++)
-                        {
-                            if (pointNodes.item(k).getNodeName().equals("wml2:MeasurementTVP")) {
-                                String time = pointNodes.item(k).getChildNodes().item(1).getTextContent();
-                                String value = pointNodes.item(k).getChildNodes().item(3).getTextContent();
-                                System.out.println("Wind Speed at " + time + ": " + value + " m/s");
-
-                                windTime = time;
-                                windSpeed = Float.parseFloat(value);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return new Measurement<Float>(windSpeed, windTime);
+        return new Measurement<>(measuredValue, measurementTime);
     }
 
     private WeatherStatus getWeather(String city, String starttime, String endtime) throws Exception  {
@@ -101,8 +72,8 @@ public class WeatherService {
         assert xmlData != null;
         Document document = builder.parse(new java.io.ByteArrayInputStream(xmlData.getBytes()));
 
-        Measurement<Float> temperature = parseTemperature(document);
-        Measurement<Float> windSpeed = parseWindSpeed(document);
+        Measurement<Float> temperature = parseMeasurement(document, "temperature");
+        Measurement<Float> windSpeed = parseMeasurement(document, "windspeedms");
 
         String finalDescription = "Weather in " + city + ". Temperature (C) at " + temperature.getTime() + ", wind speed (m/s) at " + windSpeed.getTime();
 
