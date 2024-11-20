60d806c87d7243d693a81573958072f297997c9d
dvrisa <38985578+Riku-S@users.noreply.github.com>
2024-11-20 15:07:57 +0200

    Add react-cauge-component to display transportation speed

--- src/main/frontend/src/TransportDisplay/TransportDisplay.js
+++ src/main/frontend/src/TransportDisplay/TransportDisplay.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { DataContext } from '../context/DataContext';
+import GaugeComponent from 'react-gauge-component';
 import "./TransportDisplay.css";
 
 const TransportDisplay = () => {
@@ -14,9 +15,34 @@ const TransportDisplay = () => {
         <h3>Transport in {city}</h3>
         {transport && transport.trafficStatus && transport.trafficStatus != "" ? (
           <div>
+            <div class={'cauge-container'}>
+              <GaugeComponent 
+                arc={{
+                  subArcs: [
+                    {
+                      limit: 75,
+                      color: '#f02020',
+                      showTick: true
+                    },
+                    {
+                      limit: 90,
+                      color: '#f0f020',
+                      showTick: true
+                    },
+                    {
+                      limit: 100,
+                      color: '#20f020',
+                      showTick: true
+                    },
+                  ]
+                }}
+                value={Math.round(transport.freeFlowPercentage)}
+              />
+            </div>
             <p>The traffic in the city goes <b>{Math.round(transport.freeFlowPercentage)}%</b> of its free flow speed.</p>
             <p class={transport.freeFlowPercentage < 75 ? 'slow' : (transport.freeFlowPercentage < 90 ? 'platooning' : 'fast')}>The traffic in the city is <b>{transport.trafficStatus}</b>.</p>
           </div>
+          
         ) : (
           <p>No transport data available.</p>
         )}
